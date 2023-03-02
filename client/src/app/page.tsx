ca1011d12087cbf16955f50abf5aba31e8a941bf
Mutsuki Minezaki <m.minezaki@4nonome.com>
2023-03-02 20:26:15 +0900

    frontend環境構築

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>Next.js-DRF-Blog</Head>
+      <h1 className="text-9xl">TailwindCSS</h1>
+    </div>
+  );
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "./src/app/page.{js,ts,jsx,tsx}",
+    ".src/app/components/**/*{js,ts,jsx,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
